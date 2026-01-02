de70de013cf4749eb30cd07ee2bf981e19828782
0-don <don.cryptus@gmail.com>
2026-01-02 14:37:37 +0100

    chore: update version log to v1.0.3 in app component

--- src/app.tsx
+++ src/app.tsx
@@ -21,7 +21,7 @@ import {
 import { useTrashbinStore } from "./store/trashbin-store";
 
 function App() {
-  console.log("trashbin+ loaded v1.0.2");
+  console.log("trashbin+ loaded v1.0.3");
 
   const trashbinStore = useTrashbinStore();
 
